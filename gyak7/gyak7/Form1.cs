e59326b6427fc88596747622cee1c3883a481695
Zsombor Magyar <zsombor.magyar@stud.uni-corvinus.hu>
2022-11-11 23:05:57 +0100

    adatok betolt

--- gyak7/gyak7/Form1.cs
+++ gyak7/gyak7/Form1.cs
@@ -4,17 +4,23 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
 
 namespace gyak7
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Tick.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
