e59370c4d00646d311a62117fe4c0d9baa6702ff
Zsombor Magyar <zsombor.magyar@stud.uni-corvinus.hu>
2022-12-02 20:48:11 +0100

    lista függvények

--- 9hét/9hét/Form1.cs
+++ 9hét/9hét/Form1.cs
@@ -37,24 +37,70 @@ namespace _9hét
                         BirthYear = int.Parse(line[0]),
                         Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
                         NbrOfChildren = int.Parse(line[2])
-
-
-
                     }
                          );
-                
-                
-                
+                            
                 }
-            
-            
-            
-            
+
             }
 
             return population;
 
+        }
 
+
+        public List<BirthProbability> GetBirthProbabilities(string csvpath)
+        { List<BirthProbability> birthProbabilities = new List<BirthProbability>();
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    birthProbabilities.Add(new BirthProbability()
+                    {
+                        Age = int.Parse(line[0]),
+                        NbrOfChildren = int.Parse(line[1]),
+                        Probability = double.Parse(line[2])
+
+                    }
+                    );
+                }  
+
+
+
+
+            }   
+        
+                return birthProbabilities;
+        
+        }
+
+
+
+        public List<DeathProbability> GetDeathProbabilities(string csvpath)
+        {
+            List<DeathProbability> deathProbabilities = new List<DeathProbability>();
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    deathProbabilities.Add(new DeathProbability()
+                    {
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[0]),
+                        Age = int.Parse(line[1]),
+                        Probability = double.Parse(line[2])
+
+                    }
+                    );
+                }
+
+
+
+
+            }
+
+            return deathProbabilities;
 
         }
     }
