0562742f6bd403042cc1ed7e97e551ff251f3748
Zsombor Magyar <zsombor.magyar@stud.uni-corvinus.hu>
2022-12-09 19:11:33 +0100

    Verseny felépítés

--- hét10/hét10/Form1.cs
+++ hét10/hét10/Form1.cs
@@ -79,8 +79,13 @@ namespace hét10
             gc.Start();
         }
 
-
-
-
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
+        }
     }
 }
