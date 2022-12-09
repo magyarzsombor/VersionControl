7140f84fe1f13ca6d2d9f8d3a74af3eaf520b047
Zsombor Magyar <zsombor.magyar@stud.uni-corvinus.hu>
2022-12-09 19:07:22 +0100

    új generáció

--- hét10/hét10/Form1.cs
+++ hét10/hét10/Form1.cs
@@ -36,8 +36,9 @@ namespace hét10
                 gc.AddPlayer(nbrOfSteps);
 
             }
- 
+
             gc.Start();
+
         }
 
 
@@ -47,6 +48,25 @@ namespace hét10
             label1.Text = string.Format(
     "{0}. generáció",
     generation);
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
 
 
